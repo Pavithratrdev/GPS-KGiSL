b68e5c2e37471a272f668820cf81d95f3641d41a
jeeva2334 <jeeva.v910@gmail.com>
2024-05-30 10:44:22 +0530

    added multiple domains and filter

--- Frontend/src/components/GuruForm.js
+++ Frontend/src/components/GuruForm.js
@@ -41,39 +41,277 @@ const GuruForm = () => {
   const years = ['1st Year', '2nd Year', '3rd Year', '4th Year'];
   const domains = ['AI/ML', "Blockchain", "Cloud and Devops", "Cyber Security", "Full Stack", "Java"];
   const gurus = [
-    { "Name": "Dr. S.Kalpana", "Designation": "HoD / ASP", "Company": "AI&DS", "Sector": "Edu", "Domain": "AI/ML" },
-    { "Name": "Mr.Sathish R", "Designation": "Asst Prof", "Company": "AI&DS", "Sector": "Edu", "Domain": "AI/ML" },
-    { "Name": "Mr.Rajasekaran. S", "Designation": "ASP", "Company": "IT", "Sector": "Edu", "Domain": "Cloud and Devops" },
-    { "Name": "Mr.V. Vivekanandan", "Designation": "Asst Prof", "Company": "CSE", "Sector": "Edu", "Domain": "Cloud and Devops" },
-    { "Name": "Mrs. Geetha. V", "Designation": "Asst Prof", "Company": "CSE", "Sector": "Edu", "Domain": "Full Stack" },
-    { "Name": "Dr.Leena B", "Designation": "Asst Prof", "Company": "ECE", "Sector": "Edu", "Domain": "AI/ML" },
-    { "Name": "Dr. C. Venkatesh", "Designation": "Professor", "Company": "ECE", "Sector": "Edu", "Domain": "Java" },
-    { "Name": "Dr.Kamalakannan.S", "Designation": "ASP", "Company": "ECE", "Sector": "Edu", "Domain": "Java" },
-    { "Name": "Dr. S.K. Mydhili", "Designation": "Professor", "Company": "ECE", "Sector": "Edu", "Domain": "AI/ML" },
-    { "Name": "Mr.E. Joel Anandraj", "Designation": "Asst Prof", "Company": "IT", "Sector": "Edu", "Domain": "Cyber Security" },
-    { "Name": "Dr. P.Krishna Priya", "Designation": "Director", "Company": "KG Genius Labs", "Sector": "Edu", "Domain": "AI/ML" },
-    { "Name": "Ms.Pavithra. T.R", "Designation": "Director", "Company": "Metazord", "Sector": "Edu", "Domain": "Blockchain" },
-    { "Name": "Mr. Navaneeth.M", "Designation": "Director", "Company": "KGX", "Sector": "Edu", "Domain": "AI/ML" },
-    { "Name": "Ms. Ramya Vellingiri", "Designation": "Director & Principal Architect", "Company": "KGISL", "Sector": "Industry", "Domain": "Full Stack" },
-    { "Name": "Mr.Vinith Menon", "Designation": "Vice President & Regional Head - MEA", "Company": "KGISL", "Sector": "Industry", "Domain": "Java" },
-    { "Name": "Mr.Saranyan Umapathy", "Designation": "Vice President - Global Business Development", "Company": "KGISL", "Sector": "Industry", "Domain": "Java" },
-    { "Name": "Mr. Anand Pulijal", "Designation": "Director - Business Development", "Company": "KG Invicta Services Pvt LTD", "Sector": "Industry", "Domain": "Java" },
-    { "Name": "Mr. Santhosh Sadashivan", "Designation": "Director - Products & Solutions", "Company": "KG Invicta Services Pvt LTD", "Sector": "Industry", "Domain": "Full Stack" },
-    { "Name": "Ms. Usha Venugopal", "Designation": "Operations Manager", "Company": "KGM", "Sector": "Industry", "Domain": "Blockchain" },
-    { "Name": "Mr.Shravan Kumar R", "Designation": "Deputy Manager - Program Planning & Production Control", "Company": "BMW Plant Chennai", "Sector": "Industry", "Domain": "Cloud and Devops" },
-    { "Name": "Dr.Chellappa Gopalakrishnan", "Designation": "CEO", "Company": "Genomatics Pvt Ltd", "Sector": "Industry", "Domain": "Java" },
-    { "Name": "Vishwanath Surendiran", "Designation": "CEO", "Company": "Steam-A", "Sector": "Industry", "Domain": "Java" },
-    { "Name": "Mr.Dinesh Tantri", "Designation": "Founder", "Company": "Digithos", "Sector": "Industry", "Domain": "Java" },
-    { "Name": "Thamizharasu C", "Designation": "Manager - Strategic Projects", "Company": "Quadra Systems", "Sector": "Industry", "Domain": "Cloud and Devops" },
-    { "Name": "Mr.Chinnasamy", "Designation": "Founder", "Company": "Centillion Labs", "Sector": "Industry", "Domain": "Java" },
-    { "Name": "Vishnu Prasad KC", "Designation": "Sr. Manager - HR", "Company": "NCC Ltd", "Sector": "Industry", "Domain": "Cyber Security" },
-    { "Name": "Mr.Anantha Raman", "Designation": "Director - Projects", "Company": "ZakApps", "Sector": "Industry", "Domain": "Cyber Security" },
-    { "Name": "Indu Renganathan", "Designation": "HR Generalist", "Company": "Glenwood Systems", "Sector": "Industry", "Domain": "Cyber Security" },
-    { "Name": "Mr.Saravanan Theckyam", "Designation": "Product Lead", "Company": "Google", "Sector": "Industry", "Domain": "AI/ML" },
-    { "Name": "Mr.Ramesh Gopal", "Designation": "Co-Founder", "Company": "Colors of India", "Sector": "Industry", "Domain": "Blockchain" },
-    { "Name": "Mr.Archan Choudhury", "Designation": "Founder", "Company": "Blackperl Academy", "Sector": "Industry", "Domain": "Cyber Security" },
-    { "Name": "Mr.Sandeep Joshi", "Designation": "Technologist", "Company": "Ex- Amazon", "Sector": "Industry", "Domain": "Cloud and Devops" }
+    {
+      "id": 1,
+      "Name": "Dr. S.Kalpana",
+      "Designation": "HoD / ASP",
+      "Company": "AI&DS",
+      "Sector": "Edu",
+      "Domain": ["AI & ML"]
+    },
+    {
+      "id": 2,
+      "Name": "Mr.Sathish R",
+      "Designation": "Asst Prof",
+      "Company": "AI&DS",
+      "Sector": "Edu",
+      "Domain": ["AI & ML"]
+    },
+    {
+      "id": 3,
+      "Name": "Mr.Rajasekaran. S",
+      "Designation": "ASP",
+      "Company": "IT",
+      "Sector": "Edu",
+      "Domain": ["Cloud & Dev Ops"]
+    },
+    {
+      "id": 4,
+      "Name": "Mr.V. Vivekanandan",
+      "Designation": "Asst Prof",
+      "Company": "CSE",
+      "Sector": "Edu",
+      "Domain": ["Cloud & Dev Ops"]
+    },
+    {
+      "id": 5,
+      "Name": "Mrs. Geetha. V",
+      "Designation": "Asst Prof",
+      "Company": "CSE",
+      "Sector": "Edu",
+      "Domain": [""]
+    },
+    {
+      "id": 6,
+      "Name": "Dr.Leena B",
+      "Designation": "Asst Prof",
+      "Company": "ECE",
+      "Sector": "Edu",
+      "Domain": ["ML & Image Processing"]
+    },
+    {
+      "id": 7,
+      "Name": "Dr. C. Venkatesh",
+      "Designation": "Professor",
+      "Company": "ECE",
+      "Sector": "Edu",
+      "Domain": ["Wireless Communication"]
+    },
+    {
+      "id": 8,
+      "Name": "Dr.Kamalakannan.S",
+      "Designation": "ASP",
+      "Company": "ECE",
+      "Sector": "Edu",
+      "Domain": ["Wireless Communication"]
+    },
+    {
+      "id": 9,
+      "Name": "Dr. S.K. Mydhili",
+      "Designation": "Professor",
+      "Company": "ECE",
+      "Sector": "Edu",
+      "Domain": ["Embedded & Machine Learning"]
+    },
+    {
+      "id": 10,
+      "Name": "Mr.E. Joel Anandraj",
+      "Designation": "Asst Prof",
+      "Company": "IT",
+      "Sector": "Edu",
+      "Domain": ["Cyber Security"]
+    },
+    {
+      "id": 11,
+      "Name": "Dr. P.Krishna Priya",
+      "Designation": "Director",
+      "Company": "KG Genius Labs",
+      "Sector": "Edu",
+      "Domain": ["Full Stack", "Mobile Application"]
+    },
+    {
+      "id": 12,
+      "Name": "Ms.Pavithra. T.R",
+      "Designation": "Director",
+      "Company": "Metazord",
+      "Sector": "Edu",
+      "Domain": ["Block Chain", "Full Stack", "Cloud & Dev Ops"]
+    },
+    {
+      "id": 13,
+      "Name": "Mr. Navaneeth.M",
+      "Designation": "Director",
+      "Company": "KGX",
+      "Sector": "Edu",
+      "Domain": ["AI & ML", "Full Stack", "Embedded"]
+    },
+    {
+      "id": 14,
+      "Name": "Ms. Ramya Vellingiri",
+      "Designation": "Director & Principal Architect",
+      "Company": "KGISL",
+      "Sector": "Industry",
+      "Domain": ["Python", "AI & ML"]
+    },
+    {
+      "id": 15,
+      "Name": "Mr.Vinith Menon",
+      "Designation": "Vice President & Regional Head - MEA",
+      "Company": "KGISL",
+      "Sector": "Industry",
+      "Domain": ["Networking"]
+    },
+    {
+      "id": 16,
+      "Name": "Mr.Saranyan Umapathy",
+      "Designation": "Vice President - Global Business Development",
+      "Company": "KGISL",
+      "Sector": "Industry",
+      "Domain": ["Project Management"]
+    },
+    {
+      "id": 17,
+      "Name": "Mr. Anand Pulijal",
+      "Designation": "Director - Business Development",
+      "Company": "KG Invicta Services Pvt LTD",
+      "Sector": "Industry",
+      "Domain": ["Sales & Marketing - Digital"]
+    },
+    {
+      "id": 18,
+      "Name": "Mr. Santhosh Sadashivan",
+      "Designation": "Director - Products & Solutions",
+      "Company": "KG Invicta Services Pvt LTD",
+      "Sector": "Industry",
+      "Domain": ["Full stack", "Database Management", "Analytics"]
+    },
+    {
+      "id": 19,
+      "Name": "Ms. Usha Venugopal",
+      "Designation": "Operations Manager",
+      "Company": "KGM",
+      "Sector": "Industry",
+      "Domain": ["AWS", "Digital Marketing"]
+    },
+    {
+      "id": 20,
+      "Name": "Mr.Shravan Kumar R",
+      "Designation": "Deputy Manager - Program Planning & Production Control",
+      "Company": "BMW Plant Chennai",
+      "Sector": "Industry",
+      "Domain": ["Project Management"]
+    },
+    {
+      "id": 21,
+      "Name": "Dr.Chellappa Gopalakrishnan",
+      "Designation": "CEO",
+      "Company": "Genomatics Pvt Ltd",
+      "Sector": "Industry",
+      "Domain": ["Bio Informatics", "Big Data"]
+    },
+    {
+      "id": 22,
+      "Name": "Vishwanath Surendiran",
+      "Designation": "CEO",
+      "Company": "Steam-A",
+      "Sector": "Industry",
+      "Domain": ["Web Application", "Mobile Application", "Communication Protocal"]
+    },
+    {
+      "id": 23,
+      "Name": "Mr.Dinesh Tantri",
+      "Designation": "Founder",
+      "Company": "Digithos",
+      "Sector": "Industry",
+      "Domain": ["Software Engineering", "Cyber Security"]
+    },
+    {
+      "id": 24,
+      "Name": "Thamizharasu C",
+      "Designation": "Manager - Strategic Projects",
+      "Company": "Quadra Systems",
+      "Sector": "Industry",
+      "Domain": ["Application Development using Power App & Power Automate", "Deployment of Microsoft Products"]
+    },
+    {
+      "id": 25,
+      "Name": "Mr.Chinnasamy",
+      "Designation": "Founder",
+      "Company": "Centillion Labs",
+      "Sector": "Industry",
+      "Domain": ["Software Development", "Programming"]
+    },
+    {
+      "id": 26,
+      "Name": "Vishnu Prasad KC",
+      "Designation": "Sr. Manager - HR",
+      "Company": "NCC Ltd",
+      "Sector": "Industry",
+      "Domain": ["HR"]
+    },
+    {
+      "id": 27,
+      "Name": "Mr.Anantha Raman",
+      "Designation": "Director - Projects",
+      "Company": "ZakApps",
+      "Sector": "Industry",
+      "Domain": ["Software Engineering", "Programming Languages"]
+    },
+    {
+      "id": 28,
+      "Name": "Indu Renganathan",
+      "Designation": "HR Generalist",
+      "Company": "Glenwood Systems",
+      "Sector": "Industry",
+      "Domain": ["HR"]
+    },
+    {
+      "id": 29,
+      "Name": "Mr.Saravanan Theckyam",
+      "Designation": "Product Lead",
+      "Company": "Google",
+      "Sector": "Industry",
+      "Domain": ["Embedded Systems", "AI & ML"]
+    },
+    {
+      "id": 30,
+      "Name": "Mr.Ramesh Gopal",
+      "Designation": "Co-Founder",
+      "Company": "Colors of India",
+      "Sector": "Industry",
+      "Domain": ["Block Chain"]
+    },
+    {
+      "id": 31,
+      "Name": "Mr.Archan Choudhury",
+      "Designation": "Founder",
+      "Company": "Blackperl Academy",
+      "Sector": "Industry",
+      "Domain": ["Cyber Security"]
+    },
+    {
+      "id": 32,
+      "Name": "Mr.Sandeep Joshi",
+      "Designation": "Technologist",
+      "Company": "Ex- Amazon",
+      "Sector": "Industry",
+      "Domain": ["Cloud & Dev Ops"]
+    }
   ];
+  
+  const getAllDomains = () => {
+    const domainsSet = new Set();
+    gurus.forEach(guru => {
+      guru.Domain.forEach(domain => {
+        if (domain) {
+          domainsSet.add(domain);
+        }
+      });
+    });
+    return Array.from(domainsSet).sort();
+  };
+  
+  const allDomains = getAllDomains();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -153,7 +391,7 @@ const GuruForm = () => {
   };
 
   const filterGurusByDomain = (domain) => {
-    return gurus.filter(guru => guru.Domain === domain);
+    return gurus.filter(guru => guru.Domain.includes(domain));
   };
 
   return (
@@ -259,7 +497,7 @@ const GuruForm = () => {
                 required
               >
                 <option value="">Select</option>
-                {domains.map(domain => (
+                {allDomains.map(domain => (
                   <option key={domain} value={domain}>{domain}</option>
                 ))}
               </select>
